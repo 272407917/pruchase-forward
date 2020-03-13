05cf47874ee4248260173d0c9c0f1d85df3fcb7d
272407917 <272407917@qq.com>
2020-03-13 13:23:04 +0800

    前台项目（完成）

--- src/forward/controller/GoodSvl.java
+++ src/forward/controller/GoodSvl.java
@@ -1,5 +1,6 @@
 package forward.controller;
 
+import forward.pojo.CarBean;
 import forward.pojo.Goods;
 import forward.service.GoodService;
 import forward.service.serviceImp.GoodServiceImp;
@@ -11,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -35,7 +37,121 @@ public class GoodSvl extends HttpServlet {
             openMain(request,response,pageNow);
         }else if (reqType.equals("downImg")){
             downImg(request,response);
+        }else if (reqType.equals("addCar")){
+            addCar(request,response);
+        }else if (reqType.equals("delGood")){
+            delGood(request,response);
+        }else if (reqType.equals("clearCar")){
+            clearCar(request,response);
+        }else if (reqType.equals("modCar")){
+            modCar(request,response);
+        }else if (reqType.equals("flow")){
+            openFlow(request,response);
+        }else if (reqType.equals("calculator")){
+            calculator(request,response);
         }
+    }
+
+    //结算中心
+    private void calculator(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession carSession = request.getSession();
+        //得到carSession中存放的购物车对象
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        double blance=carBean.getBlance();
+        try {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().print("<script>alert('支付成功，支付金额：￥"+blance+"')</script>");
+            response.getWriter().print(
+            "<script>"+
+            "if (window.confirm('是否返回主页面')) {"+
+                "document.location.href='goodSvl?reqType=main&pageNow=1'"+
+            "}"+
+	        "</script>"
+            );
+            response.getWriter().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //查看购物车连接
+    private void openFlow(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession carSession = request.getSession();
+        //得到carSession中存放的购物车对象
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        //调用购物车商品信息展示方法
+        if (carBean!=null){
+            genericCarData(request,response,carBean,carSession);
+        }else {
+            request.getRequestDispatcher("flow.jsp").forward(request,response);
+        }
+
+    }
+
+    //更新购物车
+    private void modCar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //得到购物车中商品id和数量amount，有多个商品所以是数组
+        String[] goodids = request.getParameterValues("goodids");
+        String[] amounts = request.getParameterValues("amounts");
+        //将字符串数组里的数据转换成整数型
+        Integer[] goodids_int=new Integer[goodids.length];
+        Integer[] amounts_int=new Integer[amounts.length];
+        for (int i = 0; i < goodids.length; i++) {
+            goodids_int[i]=Integer.valueOf(goodids[i]);
+            amounts_int[i]=Integer.valueOf(amounts[i]);
+        }
+        HttpSession carSession = request.getSession();
+        //得到carSession中存放的购物车对象
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        //更新购物车
+        carBean.modGood(goodids_int,amounts_int);
+        //调用购物车商品信息展示方法
+        genericCarData(request,response,carBean,carSession);
+    }
+
+    //清空购物车
+    private void clearCar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession carSession = request.getSession();
+        //得到carSession中存放的购物车对象
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        carBean.clearCar();
+        //调用购物车商品信息展示方法
+        genericCarData(request,response,carBean,carSession);
+    }
+
+    //删除商品
+    private void delGood(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String goodid = request.getParameter("goodid");
+        Integer id=Integer.valueOf(goodid);
+        HttpSession carSession = request.getSession();
+        //得到carSession中存放的购物车对象
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        carBean.removeGood(id);
+        //调用购物车商品信息展示方法
+        genericCarData(request,response,carBean,carSession);
+    }
+
+
+    //添加商品进入购物车
+    private void addCar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer goodid = Integer.valueOf(request.getParameter("goodid"));
+        HttpSession carSession = request.getSession();
+        //从carSession中取出购物车对象，若为null说明还没有添加购物车
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        if (carBean==null){
+            carBean=new CarBean();//创建购物车对象
+        }
+        carBean.addGood(goodid);
+        genericCarData(request,response,carBean,carSession);
+    }
+
+    //通过购物车car中的id查询出商品信息，并在页面中展示出来
+    private void genericCarData(HttpServletRequest request, HttpServletResponse response,CarBean carBean,HttpSession carSession) throws ServletException, IOException {
+        //将得到的购物车对象放入carSession中存放
+        carSession.setAttribute("car",carBean);
+        List<Goods> carGoodData = goodService.findCarData(carBean);
+        request.setAttribute("carGoodData",carGoodData);
+        request.getRequestDispatcher("flow.jsp").forward(request,response);
     }
 
     //向用户端传输商品图片，因为图片在安全目录upload下所以不能直接访问，只能通过控制器传输图片
@@ -85,6 +201,17 @@ public class GoodSvl extends HttpServlet {
             }
             goodsByType = goodService.findGoodsByType(goodType, pageNow, pageSize);
         }
+        HttpSession carSession = request.getSession();
+        //得到carSession中存放的购物车对象
+        CarBean carBean=(CarBean) carSession.getAttribute("car");
+        int amounts=0;
+        double blance=0;
+        if (carBean!=null){
+            amounts=carBean.getAllAmounts();
+            blance=carBean.getBlance();
+        }
+        request.setAttribute("allAmount",amounts);
+        request.setAttribute("Blance",blance);
 
         request.setAttribute("goodTypeList",allGoodType);
         request.setAttribute("goodsByTypeList",goodsByType);
